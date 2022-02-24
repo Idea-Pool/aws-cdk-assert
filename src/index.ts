e8b2e31ed9d72b71ce545a67ce828cc57e121905
Laszlo Szikszai <sziklaszlo@gmail.com>
2022-02-24 17:14:28 +0100

    Fixed route53

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './advanced-matcher';
 export * from './advanced-template';
 export * from './cloudfront';
 export * from './resource-assert';
+export * from './route53';
 export * from './s3';
 export * from './types';
