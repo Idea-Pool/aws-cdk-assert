a01b482346fe43bf5a5bb96c0da3b081e8fa7020
Laszlo Szikszai <sziklaszlo@gmail.com>
2022-02-24 17:10:32 +0100

    Updated workflow

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,8 @@
-import { noop } from "../src";
+import { Resource, ResourceTypes } from "../src";
 
 describe("aws-cdk-assert", () => {
-  test("should do noop", () => {
-    noop();
-  });
+  test('should do anything', () => {
+    const r = new Resource(ResourceTypes.CDK_METADATA, null, {})
+    console.log(r);
+  })
 });
